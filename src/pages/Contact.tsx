34dc61c6a47cc3d7ba12b1a85a933497dc45aec8
Zii Engelhardt <ziiengelhardt@gmail.com>
2026-01-01 14:43:43 -0800

    linting changes to pages, remove unused css file from template

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,12 +1,16 @@
 const Contact = () => (
   <div>
     <h1>Get in Touch</h1>
-    <p>I’d love to hear about what you’re working on and how I could help. I’m currently 
-    looking for a new role and am open to a wide range of opportunities. My preference 
-    would be to find a position in a company in London. But I’m also happy to hear about 
-    opportunites that don’t fit that description. I’m a hard-working and positive person 
-    who will always approach each task with a sense of purpose and attention to detail. 
-    Please do feel free to check out my online profiles below and get in touch using the form.</p>
+    <p>
+      I'd love to hear about what you're working on and how I could help. I'm
+      currently looking for a new role and am open to a wide range of
+      opportunities. My preference would be to find a position in a company in
+      London. But I'm also happy to hear about opportunites that don't fit that
+      description. I'm a hard-working and positive person who will always
+      approach each task with a sense of purpose and attention to detail. Please
+      do feel free to check out my online profiles below and get in touch using
+      the form.
+    </p>
 
     <h2>Contact Me</h2>
     <form>
--- src/pages/Fylo.tsx
+++ src/pages/Fylo.tsx
@@ -1,25 +1,39 @@
 const Fylo = () => (
   <div>
     <h1>Fylo</h1>
-    <p>This project was built in pure HTML and CSS. I had mobile and desktop designs to 
-    work to and built it so that it was fully-responsive. I took a mobile-first approach 
-    and used modern CSS like Flexbox and Grid for layout purposes.</p>
+    <p>
+      This project was built in pure HTML and CSS. I had mobile and desktop
+      designs to work to and built it so that it was fully-responsive. I took a
+      mobile-first approach and used modern CSS like Flexbox and Grid for layout
+      purposes.
+    </p>
 
     <p>Interaction Design / Front End Development</p>
     <p>HTML / CSS</p>
     <p>Visit Website</p>
 
     <h2>Project Background</h2>
-    <p>This project was a front-end  challenge from Frontend Mentor. It’s a platform that 
-    enables you to practice building websites to a design and project brief. Each challenge 
-    includes mobile and desktop designs to show how the website should look at different 
-    screen sizes. Creating these projects has helped me refine my workflow and solve 
-    real-world coding problems. I’ve learned something new with each project, helping 
-    me to improve and adapt my style.</p>
+    <p>
+      This project was a front-end challenge from Frontend Mentor. It's a
+      platform that enables you to practice building websites to a design and
+      project brief. Each challenge includes mobile and desktop designs to show
+      how the website should look at different screen sizes. Creating these
+      projects has helped me refine my workflow and solve real-world coding
+      problems. I've learned something new with each project, helping me to
+      improve and adapt my style.
+    </p>
 
     <h2>Static Previews</h2>
-    <p>Insure<br/>Previous Project</p>
-    <p>Manage<br/>Next Project</p>
+    <p>
+      Insure
+      <br />
+      Previous Project
+    </p>
+    <p>
+      Manage
+      <br />
+      Next Project
+    </p>
 
     <p>Interested in doing a project together?</p>
     <p>Contact Me</p>
