7fb6138cac2390437a0a8cfd80d66b17cb9b5553
Zii Engelhardt <ziiengelhardt@gmail.com>
2026-01-15 14:07:07 -0800

    add social link icons, css fixes

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,15 +1,18 @@
 import { Link } from "react-router-dom";
 import Logo from "../../assets/images/logo.svg?react";
+import GitHub from "../../assets/images/icons/github.svg?react";
+import LinkedIn from "../../assets/images/icons/linkedin.svg?react";
+import Twitter from "../../assets/images/icons/twitter.svg?react";
 import "./Footer.css";
 
 const Footer = () => (
   <footer>
     <div className="content">
-      <Link to="/" className="logo">
+      <Link to="/" className="icon" aria-label="Homepage">
         <Logo />
       </Link>
       <nav>
-        <ul>
+        <ul className="left">
           <li>
             <Link to="/">Home</Link>
           </li>
@@ -21,6 +24,37 @@ const Footer = () => (
           </li>
         </ul>
       </nav>
+      <div className="social-links">
+        <ul className="right">
+          <li>
+            <Link
+              to="https://www.github.com"
+              className="icon"
+              aria-label="GitHub"
+            >
+              <GitHub />
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="https://www.twitter.com"
+              className="icon"
+              aria-label="Twitter"
+            >
+              <Twitter />
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="https://www.linkedin.com"
+              className="icon"
+              aria-label="LinkedIn"
+            >
+              <LinkedIn />
+            </Link>
+          </li>
+        </ul>
+      </div>
     </div>
   </footer>
 );
